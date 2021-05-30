e7a90f25045fb001606aaa146c9d6ba2b03b3103
Sean Moriarty <hippiehaircut@gmail.com>
2021-05-30 11:16:07 -0700

    added onclick events for buttons

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,19 @@
-jQuery("i").fitText();
+
+
+rockBtn.onclick = () => {
+    playerChoice("rock");
+};
+paperBtn.onclick = () => {
+    playerChoice("paper");
+};
+scissorsBtn.onclick = () => {
+    playerChoice("scissors");
+};
+
+
+
+function playerChoice(choice) {
+    console.log(choice);
+}
+
+
